0fda2aedbe2d1b4cfb3e0cded841a256c5a3af70
taylor drew <taylordrew4u@gmail.com>
2026-03-20 12:02:18 -0400

    Remove OpenAI key and service, fix Info.plist to use only iCloud.666bit

--- thebitbinder/Services/OpenAIJokeImportService.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-@available(*, unavailable, message: "OpenAI has been removed from BitBinder. Use LocalJokeImportAIService or AppleFoundationModelsJokeImportService.")
-final class OpenAIJokeImportService {
-}
